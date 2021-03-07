57b297edbb677f9bda22e5cdbc11c5d5debcef15
maksim_pesetski <maksim.pesetski@gmail.com>
2021-03-07 09:48:27 -0500

    added retryCount to client package

--- client/options.go
+++ client/options.go
@@ -3,5 +3,6 @@ package ihttp
 type Options struct {
 	URL        string             `json:"url"`
 	Headers    *map[string]string `json:"headers,omitempty"`
+	RetryCount int                `json:"retryCount"`
 	TimeoutMs  int                `json:"timeoutMs"`
 }
